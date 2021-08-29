aa3025193df1bb0f46943a54f73efeb81d7dd813
tomisofia <tomi.ulla@gmail.com>
2021-08-29 20:20:45 -0300

    rotated profile working

--- numerical/Profiles/plasma_gaussiano_sie_canto_rot.py
+++ numerical/Profiles/plasma_gaussiano_sie_canto_rot.py
@@ -9,7 +9,7 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
     """
     class to compute the physical deflection angle of a point mass, given as an Einstein radius
     """
-    param_names = ['theta_E', 'eta', 'a', 'b1', 'b2', 'c1', 'c2', 'c3', 'd1', 'd2', 'd3', 'd4', 'e1', 'e2', 'e3', 'e4', 'e5', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'psi0_plasma']
+    param_names = ['theta_E', 'eta', 'A', 'B', 'C', 'psi0_plasma','dl']
     #lower_limit_default = {'A': 0.0, 'B': 0.0, 'k':1.0}
     #upper_limit_default = {'A': 100, 'B': 100, 'k': 100}
 
@@ -18,7 +18,7 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
         super(PlasmaGaussianSIEcantorot, self).__init__()
         # alpha = 4*const.G * (mass*const.M_sun)/const.c**2/(r*const.Mpc)
 
-    def function(self, x, y, theta_E, eta, a, b1, b2, c1, c2, c3, d1, d2, d3, d4, e1, e2, e3, e4, e5, f1, f2, f3, f4, f5, f6, psi0_plasma, center_x=0, center_y=0):
+    def function(self, x, y, theta_E, eta, A, B, C, psi0_plasma, dl, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)º
@@ -27,7 +27,6 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
         :return: lensing potential
         """
 
-        
         x_ = x - center_x
         y_ = y - center_y
         a = np.sqrt(x_**2 + y_**2)
@@ -40,18 +39,20 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
             r[a > self.r_min] = a[a > self.r_min]  #in the SIS regime
             r[a <= self.r_min] = self.r_min
         
-        N_e = (a + 
-        b1*np.abs(x_) + b2*np.abs(y_) + 
-        c1*x_**2 + c2*np.abs(x_)*np.abs(y_) + c3*y_**2 + 
-        d1*np.abs(x_)**3 + d2*x_**2*np.abs(y_) + d3*np.abs(x_)*y_**2 + d4*np.abs(y_)**3 + 
-        e1*x_**4 + e2*np.abs(x_)**3*np.abs(y_) + e3*x_**2*y_**2 + e4*np.abs(x_)*np.abs(y_)**3 + e5*y_**4 + 
-        f1*np.abs(x_)**5 + f2*x_**4*np.abs(y_) + f3*np.abs(x_)**3*y_**2 + f4*x_**2*np.abs(y_)**3 + f5*np.abs(x_)*y_**4 + f6*np.abs(y_)**5)
+        
+        x__ = x_*np.pi/(180.*3600.) #convert arcsec to rad
+        y__ = y_*np.pi/(180.*3600.) 
+        x2 = x__*dl*1e-20
+        y2 = y__*dl*1e-20
+        const = 1e28
+        
+        N_e = const*A*np.exp(-x2**2/B)*np.exp(-y2**2/C)
         
         phi = theta_E*r*np.sqrt(1.-eta*np.cos(2.*theta))   +   psi0_plasma*N_e
         
         return phi
 
-    def derivatives(self, x, y, theta_E, eta, a, b1, b2, c1, c2, c3, d1, d2, d3, d4, e1, e2, e3, e4, e5, f1, f2, f3, f4, f5, f6, psi0_plasma, center_x=0, center_y=0):
+    def derivatives(self, x, y, theta_E, eta, A, B, C, psi0_plasma, dl, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)
@@ -76,24 +77,30 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
         dtheta_dy = x_/r**2
         
         # de canto |cos\phi| y r*sin\phi=sqrt( (r*sin\phi)^2+algo_chiquito^2 )
-        
+        x__ = x_*np.pi/(180.*3600.) #convert arcsec to rad
+        y__ = y_*np.pi/(180.*3600.)
+        x2 = x__*dl*1e-20
+        y2 = y__*dl*1e-20
+        const = 1e28     
+                
         
         dphi_dr = theta_E*np.sqrt(1.-eta*np.cos(2.*theta)) 
         dphi_dtheta = theta_E*r*eta*np.sin(2.*theta)/np.sqrt(1.-eta*np.cos(2.*theta))
         
-        dphiplasma_dx = psi0_plasma*(b1*np.abs(x_)/x_  + 2*c1*x_ + c2*np.abs(x_)/x_*np.abs(y_)  + 3*d1*x_**2*np.abs(x_)/x_ + 2*d2*x_*np.abs(y_) + d3*np.abs(x_)/x_*y_**2 + 4*e1*x_**3 + 3*e2*x_**2*np.abs(x_)/x_*np.abs(y_) + 2*e3*x_*y_**2 + e4*np.abs(x_)/x_*np.abs(y_)**3 +  5*f1*x_**4*np.abs(x_)/x_ + 4*f2*x_**3*np.abs(y_) + 3*f3*x**2*np.abs(x_)/x_*y_**2 + 2*f4*x_*np.abs(y_)**3 + f5*np.abs(x_)/x_*y_**4)
+        dx2_dx_ = np.pi/(180.*3600.)*dl*1e-20
+        dy2_dx_ = np.pi/(180.*3600.)*dl*1e-20
         
+        N_e = const*A*np.exp(-x2**2/B)*np.exp(-y2**2/C)
         
-        dphiplasma_dy = psi0_plasma*(b2*np.abs(y_)/y_  + c2*np.abs(x_)*np.abs(y_)/y_  + 2*c3*y_ + d2*x_**2*np.abs(y_)/y_  + 2*d3*np.abs(x_)*y_ + 3*d4*y**2*np.abs(y_)/y_  + e2*np.abs(x_)**3*np.abs(y_)/y_  + 2*e3*x_**2*y_ + 3*e4*np.abs(x_)*y_**2*np.abs(y_)/y_  + 4*e5*y_**3 + f2*x_**4*np.abs(y_)/y_  + 2*f3*np.abs(x_)**3*y_ + 3*f4*x_**2*y_**2*np.abs(y_)/y_  + 4*f5*np.abs(x_)*y_**3 + 5*f6*y_**4*np.abs(y_)/y_)
+        dphiplasma_dx = -2.*x2/B*N_e*psi0_plasma
+        dphiplasma_dy = -2.*y2/C*N_e*psi0_plasma
         
-        
-        
-        dphi_dx = dphi_dr * dr_dx + dphi_dtheta * dtheta_dx + dphiplasma_dx
-        dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy + dphiplasma_dy
+        dphi_dx = dphi_dr * dr_dx + dphi_dtheta * dtheta_dx + dphiplasma_dx*dx2_dx_
+        dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy + dphiplasma_dy*dy2_dx_
         
         return dphi_dx, dphi_dy
 
-    def hessian(self, x, y, theta_E, eta, a, b1, b2, c1, c2, c3, d1, d2, d3, d4, e1, e2, e3, e4, e5, f1, f2, f3, f4, f5, f6, psi0_plasma, center_x=0, center_y=0):
+    def hessian(self, x, y, theta_E, eta, A, B, C, psi0_plasma, dl, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)
@@ -126,6 +133,13 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
         #dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy
         # ------------------------
         
+        x__ = x_*np.pi/(180.*3600.) #convert arcsec to rad
+        y__ = y_*np.pi/(180.*3600.)
+        x2 = x__*dl*1e-20
+        y2 = y__*dl*1e-20
+        const = 1e28
+        
+
         # derivadas segundas
         
         dphi_dr_dr =0.
@@ -135,15 +149,20 @@ class PlasmaGaussianSIEcantorot(LensProfileBase):
         
         dphi_dtheta_dr = dphi_dr_dtheta
         
-        dphiplasma_dx_dx = 2*c1 + 6*d1*np.abs(x_) + 2*d2*np.abs(y_) + 12*e1*x_**2 + 6*e2*np.abs(x_)*np.abs(y_) + 2*e3*y_**2 + 20*f1*x_**2*np.abs(x_) + 12*f2*x_**2*np.abs(y_) + 6*f3*np.abs(x_)*y_**2 + 2*f4*np.abs(y_)**3 
-        dphiplasma_dy_dy = 2*c3 + 2*d3*np.abs(x_) + 6*d4*np.abs(y_) + 2*e3*x_**2 + 6*e4*np.abs(x_)*np.abs(y_)  + 12*e5*y_**2 + 2*f3*np.abs(x_)**3 + 6*f4*x_**2*np.abs(y_) + 12*f5*np.abs(x_)*y_**2 + 20*f6*y_**2*np.abs(y_)
-        dphiplasma_dx_dy = (c2*np.abs(x_)/x_*np.abs(y_)/y_ + 2*d2*x_*np.abs(y_)/y_ + 2*d3*np.abs(x_)/x_*y_ + 3*e2*x_**2*np.abs(x_)/x_*np.abs(y_)/y_ + 4*e3*x_*y_ + 3*e4*np.abs(x_)/x_*y_**2*np.abs(y_)/y_ + 4*f2*x_**3*np.abs(y_)/y_ + 6*f3*x_**2*np.abs(x_)/x_*y_ + 6*f4*x_*y_**2*np.abs(y_)/y_ + 4*f5*np.abs(x_)/x_*y_**3)
+        dx2_dx_ = np.pi/(180.*3600.)*dl*1e-20
+        dy2_dx_ = np.pi/(180.*3600.)*dl*1e-20
+        
+        N_e = const*A*np.exp(-x2**2/B)*np.exp(-y2**2/C)
+        
+        dphiplasma_dx_dx = 4.*x2**2/B**2*N_e*psi0_plasma - 2./B*N_e*psi0_plasma
+        dphiplasma_dy_dy = 4.*y2**2/C**2*N_e*psi0_plasma - 2./C*N_e*psi0_plasma
+        dphiplasma_dx_dy = 4.*x2*y2/B/C*N_e*psi0_plasma
         
         
         f_xx = dr_dx*( dphi_dr_dr*dr_dx+dphi_dr*dr_dx_dr+dphi_dr_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dr ) \
-				+ dtheta_dx* ( dphi_dtheta_dr*dr_dx+dphi_dr*dr_dx_dtheta+dphi_dtheta_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dtheta ) + dphiplasma_dx_dx
+				+ dtheta_dx* ( dphi_dtheta_dr*dr_dx+dphi_dr*dr_dx_dtheta+dphi_dtheta_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dtheta ) + dphiplasma_dx_dx*dx2_dx_**2
         f_yy = dr_dy*( dphi_dr_dr*dr_dy+dphi_dr*dr_dy_dr+dphi_dr_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dr ) \
-				+ dtheta_dy* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta )+ dphiplasma_dy_dy
+				+ dtheta_dy* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta )+ dphiplasma_dy_dy*dy2_dx_**2
         f_xy = dr_dx*( dphi_dr_dr*dr_dy+dphi_dr*dr_dy_dr+dphi_dr_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dr ) \
-				+ dtheta_dx* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta ) + dphiplasma_dx_dy
+				+ dtheta_dx* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta ) + dphiplasma_dx_dy*dx2_dx_*dy2_dx_
         return f_xx, f_xy, f_xy, f_yy
--- numerical/Profiles/sie_plasma_black_hole.py
+++ numerical/Profiles/sie_plasma_black_hole.py
@@ -1,24 +1,24 @@
-__author__ = 'gcrisnejo'
+__author__ = 'gcrisnejo,everettiantomi'
 
 
 import numpy as np
 from lenstronomy.LensModel.Profiles.base_profile import LensProfileBase
 
 
-class PlasmaGaussianSIEcanto(LensProfileBase):
+class PlasmaExponentialSIEBH(LensProfileBase):
     """
     class to compute the physical deflection angle of a point mass, given as an Einstein radius
     """
-    param_names = ['theta_E', 'eta', 'A', 'B', 'C', 'thetaz', 'psi0_plasma', 'theta_0']
+    param_names = ['theta_E', 'eta', 'A', 'B', 'C' ,'psi0_plasma', 'theta_0']
     #lower_limit_default = {'A': 0.0, 'B': 0.0, 'k':1.0}
     #upper_limit_default = {'A': 100, 'B': 100, 'k': 100}
 
     def __init__(self):
         self.r_min = 10**(-50)
-        super(PlasmaGaussianSIEcanto, self).__init__()
+        super(PlasmaExponentialSIE, self).__init__()
         # alpha = 4*const.G * (mass*const.M_sun)/const.c**2/(r*const.Mpc)
 
-    def function(self, x, y, theta_E, eta, A, B, C, thetaz, psi0_plasma, theta_0, center_x=0, center_y=0):
+    def function(self, x, y, theta_E, eta, A, B, C, psi0_plasma, theta_0, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)
@@ -37,12 +37,10 @@ class PlasmaGaussianSIEcanto(LensProfileBase):
             r = np.empty_like(a)
             r[a > self.r_min] = a[a > self.r_min]  #in the SIS regime
             r[a <= self.r_min] = self.r_min
-        
-        phi = theta_E*r*np.sqrt(1.-eta*np.cos(2.*theta))+psi0_plasma*np.exp(-(np.abs(x_)/B/theta_0)**C)*np.exp(-(y_)**2/thetaz**2 )
-        
+        phi = theta_E*r*np.sqrt(1.-eta*np.cos(2.*theta))+psi0_plasma*np.exp(-(r/B/theta_0)**C)
         return phi
 
-    def derivatives(self, x, y, theta_E, eta, A, B, C, thetaz, psi0_plasma, theta_0, center_x=0, center_y=0):
+    def derivatives(self, x, y, theta_E, eta, A, B, C, psi0_plasma, theta_0, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)
@@ -61,27 +59,20 @@ class PlasmaGaussianSIEcanto(LensProfileBase):
             r[a > self.r_min] = a[a > self.r_min]  #in the SIS regime
             r[a <= self.r_min] = self.r_min
         
-        dr_dx = x_/r #y = 3, x = 0
+        dr_dx = x_/r
         dr_dy = y_/r
         dtheta_dx = -y_/r**2
         dtheta_dy = x_/r**2
         
-        # de canto |cos\phi| y r*sin\phi=sqrt( (r*sin\phi)^2+algo_chiquito^2 )
-        
-        potencial_plasma = psi0_plasma*np.exp(-(np.abs(x_)/B/theta_0)**C)*np.exp(-(y_)**2/thetaz**2 )
-        
-        dphi_dr = theta_E*np.sqrt(1.-eta*np.cos(2.*theta)) 
+        dphi_dr = theta_E*np.sqrt(1.-eta*np.cos(2.*theta))-C/r*psi0_plasma*(r/B/theta_0)**C*np.exp(-(r/B/theta_0)**C)
         dphi_dtheta = theta_E*r*eta*np.sin(2.*theta)/np.sqrt(1.-eta*np.cos(2.*theta))
         
-        dphiplasma_dx = -potencial_plasma*C/x_*np.abs(x_)**C*(1./B/theta_0)**C
-        dphiplasma_dy = -2.*potencial_plasma*y_/thetaz**2
-        
-        dphi_dx = dphi_dr * dr_dx + dphi_dtheta * dtheta_dx + dphiplasma_dx
-        dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy + dphiplasma_dy
+        dphi_dx = dphi_dr * dr_dx + dphi_dtheta * dtheta_dx
+        dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy
         
         return dphi_dx, dphi_dy
 
-    def hessian(self, x, y, theta_E, eta, A, B, C, thetaz, psi0_plasma, theta_0, center_x=0, center_y=0):
+    def hessian(self, x, y, theta_E, eta, A, B, C, psi0_plasma, theta_0, center_x=0, center_y=0):
         """
 
         :param x: x-coord (in angles)
@@ -105,33 +96,23 @@ class PlasmaGaussianSIEcanto(LensProfileBase):
         dtheta_dx_dtheta = -np.cos(theta)/r; dtheta_dy_dtheta = -np.sin(theta)/r
         
         # derivadas primeras
-        #--------------------------
-        potencial_plasma = psi0_plasma*np.exp(-(np.abs(x_)/B/theta_0)**C)*np.exp(-(y_)**2/thetaz**2 )
         
-        dphi_dr = theta_E*np.sqrt(1.-eta*np.cos(2.*theta))
+        potencial = psi0_plasma*np.exp(-(r/B/theta_0)**C)
+        
+        dphi_dr = theta_E*np.sqrt(1.-eta*np.cos(2.*theta))-C/r*psi0_plasma*(r/B/theta_0)**C*np.exp(-(r/B/theta_0)**C)
         dphi_dtheta = theta_E*r*eta*np.sin(2.*theta)/np.sqrt(1.-eta*np.cos(2.*theta))
-        
-        #dphi_dx = dphi_dr * dr_dx + dphi_dtheta * dtheta_dx
-        #dphi_dy = dphi_dr * dr_dy + dphi_dtheta * dtheta_dy
-        # ------------------------
         
         # derivadas segundas
         
-        dphi_dr_dr =0.
-        dphi_dr_dtheta = eta*theta_E*np.sin(2.*theta)/np.sqrt(1.-eta*np.cos(2.*theta)) 
-        
+        dphi_dr_dr = potencial * C/r**2 *(r/B/theta_0)**C *(1.+C*(  (r/B/theta_0)**C-1.)  )
+        dphi_dr_dtheta = eta*theta_E*np.sin(2.*theta)/np.sqrt(1.-eta*np.cos(2.*theta))
         dphi_dtheta_dtheta = -eta*r*theta_E*(eta*(3.+np.cos(4.*theta))-4.*np.cos(2.*theta) )/2./(1.-eta*np.cos(2.*theta))**(3/2) 
-        
         dphi_dtheta_dr = dphi_dr_dtheta
         
-        dphiplasma_dx_dx = potencial_plasma*C*(1./B/theta_0)**C * np.abs(x_)**C/(x_)**2*(1.-C+C*(1./B/theta_0)**C *np.abs(x_ )**C)
-        dphiplasma_dy_dy = -2.*potencial_plasma*(thetaz**2-2.*(y_)**2)/thetaz**4
-        dphiplasma_dx_dy = 2.*C*potencial_plasma*(1./B/theta_0)**C *y_ *np.abs(x_)**C /thetaz**2 /x_
-        
         f_xx = dr_dx*( dphi_dr_dr*dr_dx+dphi_dr*dr_dx_dr+dphi_dr_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dr ) \
-				+ dtheta_dx* ( dphi_dtheta_dr*dr_dx+dphi_dr*dr_dx_dtheta+dphi_dtheta_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dtheta ) + dphiplasma_dx_dx
+				+ dtheta_dx* ( dphi_dtheta_dr*dr_dx+dphi_dr*dr_dx_dtheta+dphi_dtheta_dtheta*dtheta_dx+dphi_dtheta*dtheta_dx_dtheta )
         f_yy = dr_dy*( dphi_dr_dr*dr_dy+dphi_dr*dr_dy_dr+dphi_dr_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dr ) \
-				+ dtheta_dy* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta )+ dphiplasma_dy_dy
+				+ dtheta_dy* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta )
         f_xy = dr_dx*( dphi_dr_dr*dr_dy+dphi_dr*dr_dy_dr+dphi_dr_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dr ) \
-				+ dtheta_dx* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta ) + dphiplasma_dx_dy
+				+ dtheta_dx* ( dphi_dtheta_dr*dr_dy+dphi_dr*dr_dy_dtheta+dphi_dtheta_dtheta*dtheta_dy+dphi_dtheta*dtheta_dy_dtheta )
         return f_xx, f_xy, f_xy, f_yy
