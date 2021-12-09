33dcd68c90cc315ece3ac99cbdb1c34216a8f5fa
tomi.sofia <tomi.ulla@gmail.com>
2021-12-09 11:57:15 -0300

    exact critic line working

--- perturbative/plasma_jet_class.py
+++ perturbative/plasma_jet_class.py
@@ -40,12 +40,15 @@ class sieplasmajet(object):
         tmp_psi_plasma = psi0_plasma_num*np.exp(-(r/B/theta_0_num)**C)
         self.psi_plasma = tmp_psi_plasma
 
-        tmp_ddpsi = theta_E_g*(np.sqrt( 1. - eta*np.cos(2*phi)) - 1)
-        self.ddpsi_dr = tmp_ddpsi
+        tmp_ddpsi_dr = theta_E_g*(np.sqrt( 1. - eta*np.cos(2*phi)) - 1)
+        self.ddpsi_dr = tmp_ddpsi_dr
 
 
-        tmp_ddpsi = theta_E_g*r*eta*np.sin(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi))
-        self.ddpsi_dphi = tmp_ddpsi
+        tmp_ddpsi_dphi = theta_E_g*r*eta*np.sin(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi))
+        self.ddpsi_dphi = tmp_ddpsi_dphi
+        
+        tmp_d2dpsi_dphi2 = theta_E_g*r*eta*( 2*np.cos(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi)) - (1.-eta*np.cos(2.*phi))**(-3/2)*eta*np.sin(2*phi)**2)
+        self.d2dpsi_dphi2 = tmp_d2dpsi_dphi2
 
 
         tmp_d2psi0 = self.psi_plasma * ( - C*(C-1)/r**2*(r/B/theta_0_num)**C + (C/r*(r/B/theta_0_num)**C)**2 )
@@ -59,3 +62,42 @@ class sieplasmajet(object):
 
         self.delta_r_1 = delta_r_1
         self.delta_r_2 = delta_r_2
+
+        tmp_delta_r_criticline =  1/(1 - self.d2psi0_dr2 )*( self.ddpsi_dr + self.d2dpsi_dphi2 )
+        self.delta_r_criticline = tmp_delta_r_criticline
+        
+    def critic(self):
+        
+        theta_E_g = self.theta_E_g
+        eta = self.eta
+        psi0_plasma_num = self.psi0_plasma_num
+        theta_0_num = self.theta_0_num
+        B = self.B
+        C = self.C
+        delta_rs = self.delta_rs
+        deltab_10 = self.deltab_10
+        deltab_20 = self.deltab_20
+        
+        def g(r, *args): #psit = psitotal = psi0 + dpsi
+            phi = args[0]
+            dpsit_dr = theta_E_g*np.sqrt( 1. - eta*np.cos(2*phi)) - C/r*(r/B/theta_0_num)**C*self.psi_plasma
+            d2psit_dr2 = self.psi_plasma*( - C*(C-1)/r**2*(r/B/theta_0_num)**C + (C/r*(r/B/theta_0_num)**C)**2 )
+            dpsit_dphi = theta_E_g*r*eta*np.sin(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi))
+            d2psit_dphi2 = theta_E_g*r*eta*( 2*np.cos(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi)) - \
+                                            (1.-eta*np.cos(2.*phi))**(-3/2)*eta*np.sin(2*phi)**2)
+            d2psit_drdphi = theta_E_g*eta*np.sin(2.*phi)/np.sqrt(1.-eta*np.cos(2.*phi))  
+            tmp = 1/r*( (1 - d2psit_dr2 )*(r - dpsit_dr - 1/r*d2psit_dphi2  ) - 1/r*( 1/r*dpsit_dphi - d2psit_drdphi  )**2 )  
+            return tmp                      
+        
+        r = []                          
+        for phi in self.phi:
+            zero = root_scalar(g, args = phi , bracket=[theta_E_g*.1, theta_E_g*1.9], method='bisect')
+            r.append(zero.root)
+        
+        r = np.array(r)
+        return r 
+                                  
+                                  
+                                  
+                                  
+        
