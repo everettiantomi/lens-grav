49662a2d9ca01c4def6945d87cccfc6bc2cd2935
tomi.sofia <tomi.ulla@gmail.com>
2022-02-17 09:51:22 -0300

    checkpoint before changing ploting approach

--- perturbative/validity_class.py
+++ perturbative/validity_class.py
@@ -67,11 +67,27 @@ class sieplasma(object):
         delta_r = 1/(1 - self.d2psi0_dr2 )*(self.ddpsi_dr + deltab_10*np.cos(zeros_phi) + deltab_20*np.sin(zeros_phi) )
 
         self.delta_r = delta_r
+        
+        
+        r_ = r + delta_r
+        
+        psi = theta_E_g*r_*np.sqrt(1.-eta*np.cos(2.*zeros_phi)) - psi0_plasma_num*np.exp(-(r_/B/theta_0_num)**C)
+        psi_plasma = psi0_plasma_num*np.exp(-(r_/B/theta_0_num)**C)
+        d2psi_dr2 = psi_plasma * ( - C*(C-1)/r_**2*(r_/B/theta_0_num)**C + (C/r_*(r_/B/theta_0_num)**C)**2 )
+        dpsi_dr = theta_E_g*np.sqrt(1.-eta*np.cos(2.*zeros_phi)) - C/r_*(r_/B/theta_0_num)**C*psi0_plasma_num*np.exp(-(r_/B/theta_0_num)**C) 
+        d2psi_dphi2 = theta_E_g*r_*eta*( 2*np.cos(2.*zeros_phi)/np.sqrt(1.-eta*np.cos(2.*zeros_phi)) - (1.-eta*np.cos(2.*zeros_phi))**(-3/2)*eta*np.sin(2*zeros_phi)**2)
+        dpsi_dphi = theta_E_g*r_*eta*np.sin(2.*zeros_phi)/np.sqrt(1.-eta*np.cos(2.*zeros_phi))
+        d2psi_dphidr = theta_E_g*eta*np.sin(2.*zeros_phi)/np.sqrt(1.-eta*np.cos(2.*zeros_phi))
+        
+        
+        mu = r_*( (1 - d2psi_dr2)*(r_ - dpsi_dr - 1/r_*d2psi_dphi2) - 1/r_*(1/r_*dpsi_dphi - d2psi_dphidr )**2  )**(-1)
 
-        R = np.abs((delta_r[0]*(1 - self.d2psi0_dr2 ) - self.ddpsi_dr[0] - 1/r*self.d2dpsi_dphi2[0])/(delta_r[1]*(1 - self.d2psi0_dr2 ) - self.ddpsi_dr[1] - 1/r*self.d2dpsi_dphi2[1]))
+        R = np.abs(mu[0]/mu[1])
         self.R = R
         
-        dt = np.abs( (1 + zl)/c * Dl*Ds/Dls * r*(np.cos(zeros_phi[1])*deltab_10 + np.sin(zeros_phi[1])*deltab_20 - np.cos(zeros_phi[0])*deltab_10 - np.sin(zeros_phi[0])*deltab_20 + 1/r*self.dpsi[1] - 1/r*self.dpsi[0] ))
+        t = (1 + zl)/c*Dl*Ds/Dls*(1/2* ( (r_*np.cos(zeros_phi) - deltab_10)**2 + (r_*np.sin(zeros_phi) - deltab_20)**2  )*0 - psi )
+        
+        dt = np.abs(t[0]-t[1])/24/60/60*0.000004848136811095**2 #convert seconds to days and arcsec^2 to rad
         self.dt = dt
         
         
